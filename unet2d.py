c5d2977cb1544ceb2ba8c92533497a487a41c82c
Häkkinen Iira <hakkini2@cs-095.org.aalto.fi>
2024-01-23 16:11:33 +0200

    add batch normalization

--- unet2d.py
+++ unet2d.py
@@ -17,9 +17,11 @@ class Block(Module):
 		super(Block, self).__init__()
 		# store the convolution and RELU layers
 		self.conv = Sequential(
-			Conv2d(inChannels, outChannels, kernel_size=3, stride=1, padding=1),
+			Conv2d(inChannels, outChannels, kernel_size=3, stride=1, padding=1, bias=False),
+			BatchNorm2d(outChannels),
 			ReLU(inplace=True),	#try save some memory with inplace
-			Conv2d(outChannels, outChannels, kernel_size=3, stride=1, padding=1),
+			Conv2d(outChannels, outChannels, kernel_size=3, stride=1, padding=1, bias=False),
+			BatchNorm2d(outChannels),
 			ReLU(inplace=True)
 		)
 		
