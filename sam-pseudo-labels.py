b7adf4922805d88ca14ce752dde5d138f56a9d56
Häkkinen Iira <hakkini2@cs-095.org.aalto.fi>
2024-05-15 14:28:56 +0300

    adapt pseudo label creation for box prompt

--- sam-pseudo-labels.py
+++ sam-pseudo-labels.py
@@ -19,7 +19,8 @@ from samDataset import (
     get_point_prompt,
     center_of_mass_from_3d,
     averaged_center_of_mass,
-    compute_center_of_mass
+    compute_center_of_mass,
+    compute_bounding_boxes
 )
 from torchmetrics.functional.classification import dice
 from transformers import SamModel, SamProcessor
@@ -38,7 +39,7 @@ import config
 
 # create directory for saving the pseudomasks
 split = 'train'
-pseudo_masks_path = config.DATASET_PATH_2D + split + '_2d_pseudomasks/'
+pseudo_masks_path = config.DATASET_PATH_2D + split + '_2d_' + config.SAM_PROMPT + '_pseudomasks/'
 histograms_path = config.SAM_OUTPUT_PATH + config.SAM_PROMPT + '_prompt/' + split + '_pseudomask_histograms/'
 Path(pseudo_masks_path).mkdir(parents=True, exist_ok=True)
 Path(histograms_path).mkdir(parents=True, exist_ok=True)
@@ -73,36 +74,77 @@ def predict_masks(loader, predictor):
 
             # process image to get image embedding
             predictor.set_image(color_img)
-
-            # get list of point prompts - one for each cluster
-            center_of_mass_list = compute_center_of_mass(ground_truth_mask)
-
-            #initialize mask array 
-            mask = np.full(ground_truth_mask.shape, False, dtype=bool)
-            # initialize lists for input poitns and labels for plotting
-            input_points = []
-            input_labels = []
-
-            #loop through centers of mass, get sam's predictions for all and construct the final mask
-            for i, center_of_mass in enumerate(center_of_mass_list):
-                print(f"Center of mass for object {i + 1}: {center_of_mass}")
-                input_point = np.array([[round(center_of_mass[1]), round(center_of_mass[0])]])
-                input_label =  np.array([1])
-                input_points.append(input_point)
-                input_labels.append(input_label)
             
-                # get predicted masks
-                masks, scores, logits = predictor.predict(
-                    point_coords=input_point,
-                    point_labels=input_label,
-                    multimask_output=True,
-                )
 
-                # CHOOSE THE FIRST MASK FROM MULTIMASK OUTPUT 
-                cluster_mask = masks[0]
 
-                # add cluster to final mask
-                mask = mask | cluster_mask 
+            # Predict using point prompt
+            if config.SAM_PROMPT == 'point':
+
+                # get list of point prompts - one for each cluster
+                center_of_mass_list = compute_center_of_mass(ground_truth_mask)
+
+                #initialize mask array 
+                mask = np.full(ground_truth_mask.shape, False, dtype=bool)
+                # initialize lists for input poitns and labels for plotting
+                input_points = []
+                input_labels = []
+
+                #loop through centers of mass, get sam's predictions for all and construct the final mask
+                for i, center_of_mass in enumerate(center_of_mass_list):
+                    print(f"Center of mass for object {i + 1}: {center_of_mass}")
+                    input_point = np.array([[round(center_of_mass[1]), round(center_of_mass[0])]])
+                    input_label =  np.array([1])
+                    input_points.append(input_point)
+                    input_labels.append(input_label)
+                
+                    # get predicted masks
+                    masks, scores, logits = predictor.predict(
+                        point_coords=input_point,
+                        point_labels=input_label,
+                        multimask_output=True,
+                    )
+
+                    # CHOOSE THE FIRST MASK FROM MULTIMASK OUTPUT 
+                    cluster_mask = masks[0]
+
+                    # add cluster to final mask
+                    mask = mask | cluster_mask 
+            
+
+
+            # Predict using bounding box
+            if config.SAM_PROMPT == 'box':
+                #get a list of bounding boxes - one for each cluster
+                box_prompt_list = compute_bounding_boxes(ground_truth_mask)
+
+                #initialize mask array 
+                mask = np.full(ground_truth_mask.shape, False, dtype=bool)
+                # initialize lists for input boxes for plotting
+                input_boxes = []
+
+                #loop through clusters, get sam's predictions for all and construct the final mask
+                for i, box_prompt in enumerate(box_prompt_list):
+                    print(f'Bounding box for cluster {i+1}: bottom: ({box_prompt[0]}, {box_prompt[1]}), top: ({box_prompt[2]}, {box_prompt[3]})')
+
+                    #create input pormpt
+                    input_box = np.array(box_prompt)
+                    input_boxes.append(input_box)
+
+                    # get predicted masks
+                    cluster_mask, scores, logits = predictor.predict(
+                        point_coords=None,
+                        point_labels=None,
+                        box=input_box[None, :],
+                        multimask_output=False,
+                    )
+
+                    #add cluster to final mask
+                    mask = mask | cluster_mask
+
+
+
+
+
 
             # evaluate with dice score
             dice_pytorch = dice(torch.Tensor(mask).cpu(), ground_truth_mask.cpu(), ignore_index=0)
