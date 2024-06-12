cc60de5a2dbbc68f55dd71cc3822183a5b02b2b4
Häkkinen Iira <hakkini2@cs-095.org.aalto.fi>
2024-06-12 09:03:30 +0300

    result visualisation

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ N_TEST_SAMPLES = 10   # How many top/worst test plots are saved
 
 #organ to train/test on
 ORGAN_LIST = ['Task03_Liver', 'Task06_Lung', 'Task07_Pancreas', 'Task08_HepaticVessel', 'Task09_Spleen', 'Task10_Colon']
-ORGAN = ORGAN_LIST[3]
+ORGAN = ORGAN_LIST[0]
 
 # Training dataset for UNet
 TRAIN_DATA_LIST = ['all', 'n_random', 'n_worst']
@@ -19,11 +19,11 @@ N_TRAIN_SAMPLES = 1000
 
 # Use pseudo labels as ground truth masks for UNet training
 #NOTE: specify the prompt type of the pseudo labels as well (SAM_PROMPT)
-USE_PSEUDO_LABELS = False   # True/False
+USE_PSEUDO_LABELS = True   # True/False
 
 # SAM prompt type
 SAM_PROMPTS_LIST = ['naive_point', 'point', 'furthest_from_edges_point', 'box', 'one_box_with_points', 'box_and_point', 'box_and_background_point']
-SAM_PROMPT = SAM_PROMPTS_LIST[4]
+SAM_PROMPT = SAM_PROMPTS_LIST[3]
 
 # IF using perturbation for box prompts
 USE_NOISE_FOR_BOX_PROMPT = False
